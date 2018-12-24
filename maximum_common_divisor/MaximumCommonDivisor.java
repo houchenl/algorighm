d64f12dc032091c1d965405d4c7c57f3456e9b36
yuwantang <houchen890213@gmail.com>
2018-12-24 13:45:44 +0800

    maximum common divisor

---
+++ maximum_common_divisor/MaximumCommonDivisor.java
@@ -0,0 +1,20 @@
+
+class MaximumCommonDivisor {
+
+	/**
+	* 求最大公约数
+	*/
+	public static int mcd(int p, int q) {
+		if (q == 0) return p;
+		int r = p % q;
+		return mcd(q, r);
+	}
+
+
+	public static void main(String[] args) {
+		int result = 0;
+		result = mcd(36, 25);
+		System.out.println(result + "");
+	}
+
+}
